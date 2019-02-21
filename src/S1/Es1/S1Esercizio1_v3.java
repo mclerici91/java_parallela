677c73e8bd0443b5da096541ecaf1120ac31f3ba
Clerici <maura.clerici@student.supsi.ch>
2019-02-21 16:56:41 +0100

    Es1 - v3

---
+++ src/S1/Es1/S1Esercizio1_v3.java
@@ -0,0 +1,40 @@
+package S1.Es1;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class S1Esercizio1_v3 {
+    public static void main(final String[] args) {
+        final Collection<Thread> allThreads = new ArrayList<Thread>();
+
+        /* Creazione dei threads */
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Main: creo thread " + i);
+            Thread t = new Thread(() -> {
+                long fibo1 = 1, fibo2 = 1, fibonacci = 1;
+                for (int j = 3; j <= 700; j++) {
+                    fibonacci = fibo1 + fibo2;
+                    fibo1 = fibo2;
+                    fibo2 = fibonacci;
+                }
+                /* Stampa risultato */
+                System.out.println(t + ": " + fibonacci);
+            });
+            allThreads.add(t);
+        }
+
+        /* Avvio dei threads */
+        for (final Thread t : allThreads)
+            t.start();
+
+        /* Attendo terminazione dei threads */
+        for (final Thread t : allThreads) {
+            try {
+                System.out.println("Attendo la terminazione di " + t);
+                t.join();
+            } catch (final InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
