13801e7ddb3e4c5770c66b11f69fce4823c5b90e
maura.clerici@student.supsi.ch <139bfd41>
2019-03-05 11:37:02 +0100

    Es3 - senza protezioni

--- src/S3/S2Esercizio3.java
+++ src/S3/S2Esercizio3.java
@@ -2,16 +2,22 @@ package S3;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 class Conto {
     private int saldo;
 
     public void incrementaSaldo(int valore) {
-
+        saldo += valore;
     }
 
     public void decrementaSaldo(int valore) {
+        saldo -= valore;
+    }
 
+    public int getSaldo() {
+        return saldo;
     }
 
     public boolean empty() {
@@ -26,25 +32,50 @@ class Conto {
 class Utente implements Runnable{
     private int id;
     private final Conto contoComune;
+    private int saldo;
 
     public Utente(int id, Conto contoComune) {
         this.id = id;
         this.contoComune = contoComune;
+        this.saldo = 0;
     }
 
-    public void prelievo() {
+    public void prelievo(int valore) {
+        saldo += valore;
+        contoComune.decrementaSaldo(valore);
+    }
 
+    public int getSaldo() {
+        return saldo;
     }
 
     @Override
     public void run() {
+        Random r = new Random();
+        int prelievo;
+        System.out.println(this + " inizio operazione di prelevamento");
+        while (!contoComune.empty()) {
+            prelievo = r.nextInt((300-10) + 10);
+            if (contoComune.getSaldo() >= prelievo)
+                prelievo(prelievo);
 
+            // Simula il tempo prima di prelevare nuovamente
+            try {
+                Thread.sleep(ThreadLocalRandom.current().nextLong(1, 5));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(this + " termino operazione di prelievo");
     }
 }
 
 public class S2Esercizio3 {
     public static void main(String[] args) {
         Conto conto = new Conto();
+        int saldoIniziale = 3000;
+        int totalePrelevamenti = 0;
+        conto.incrementaSaldo(3000);
 
         List<Thread> allThreads = new ArrayList<>();
         List<Utente> allUsers = new ArrayList<>();
@@ -65,5 +96,13 @@ public class S2Esercizio3 {
                 e.printStackTrace();
             }
         }
+
+        for (Utente utente : allUsers) {
+            System.out.println(
+                    utente + ": saldo: " + utente.getSaldo());
+            totalePrelevamenti += utente.getSaldo();
+        }
+        System.out.println("Totale saldo comune dopo operazioni di prelievo: " + conto.getSaldo());
+        System.out.println("Totale prelevamenti degli utenti: " + totalePrelevamenti);
     }
 }
