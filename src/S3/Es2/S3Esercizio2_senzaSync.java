0c9b3b350fc79b566708348002dc0a57c44b0309
maura.clerici <maura.clerici@student.supsi.ch>
2019-03-12 11:04:37 +0100

    Serie 3 - Esercizio 2, senza sync

--- src/S3/Es2/S3Esercizio2_senzaSync.java
+++ src/S3/Es2/S3Esercizio2_senzaSync.java
@@ -2,11 +2,12 @@ package S3.Es2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 class Sensore implements Runnable {
     private int id;
     private int soglia;
-    public static int contatore;
+    private boolean isSuperato = false;
 
     public Sensore(int id, int soglia) {
         this.id = id;
@@ -23,11 +24,23 @@ class Sensore implements Runnable {
 
     @Override
     public void run() {
-
+        while (!isSuperato) {
+            if (soglia < S3Esercizio2_senzaSync.contatore) {
+                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                System.out.println("Sensore" + id + " - Soglia superata.");
+                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                S3Esercizio2_senzaSync.contatore = 0;
+                isSuperato = true;
+            }
+        }
     }
 }
 
 public class S3Esercizio2_senzaSync {
+
+    public static int contatore;
+
+
     public static void main(String[] args) {
 
         List<Thread> allThreads = new ArrayList<>();
@@ -35,13 +48,28 @@ public class S3Esercizio2_senzaSync {
 
         // Creazione dei sensori
         for (int i = 1; i <= 10; i++) {
-            final Sensore sensor = new Sensore(i, i*10);
+            final Sensore sensor = new Sensore(i, i * 10);
             allSensors.add(sensor);
             allThreads.add(new Thread(sensor));
             System.out.println("Creato sensore: Sensore" + sensor.getId() + " con soglia " + sensor.getSoglia());
         }
 
+        // Partenza dei sensori
         allThreads.forEach(Thread::start);
+
+        // Incremento contatore
+        while (S3Esercizio2_senzaSync.contatore <= 120) {
+
+            final int n = ThreadLocalRandom.current().nextInt(1, 9);
+            S3Esercizio2_senzaSync.contatore += n;
+            System.out.println("VALORE CONTATORE: " + S3Esercizio2_senzaSync.contatore);
+
+            try {
+                Thread.sleep(ThreadLocalRandom.current().nextLong(5, 11));
+            } catch (final InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
         for (Thread thread : allThreads) {
             try {
@@ -50,5 +78,7 @@ public class S3Esercizio2_senzaSync {
                 e.printStackTrace();
             }
         }
+
+        System.out.println("CONTATORE - VALORE MASSIMO RAGGIUNTO");
     }
 }
